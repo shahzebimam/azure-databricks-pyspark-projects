5769b70826f4c66afe7184a2b39f61c537a4b138
Shahzeb Imam <shahzebimam20@gmail.com>
2025-11-18 15:31:52 +0530

    Delete 3 - Job , Stages & Task.py

--- 3 - Job , Stages & Task.py
+++
@@ -1,8 +0,0 @@
-# Databricks notebook source
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import *
-from pyspark.sql.types import *
-
-# COMMAND ----------
-
-df = spark.read.format('csv').option('header',True).option('inferschema', True).load('/FileStore/ApacheSpark/MegaMart.csv').display()
